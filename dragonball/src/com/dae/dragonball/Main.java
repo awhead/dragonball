8844694359029feadb44aefa33a86cc68d55302d
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2019-04-17 04:38:49 +0900

    Main 수정 - 싱글플레이 용도로

--- dragonball/src/com/dae/dragonball/Main.java
+++ dragonball/src/com/dae/dragonball/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         // PlayerVO(boolean isPlayerBot, String playerName, int hp)
-        PlayerVO[] player = { new PlayerVO(false, "손오공", 3),
-                new PlayerVO(true, "피콜로bot", 3) };
+        PlayerVO[] players = { new PlayerVO(false, "손오공", 3), new PlayerVO(true, "피콜로bot", 3) };
         Play play = new Play();
-        play.play(player);
+        play.singlePlay(players);
     }
 
 }
