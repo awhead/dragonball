faafc27b957803a02ec39dc3e5867255da5c48fe
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2019-04-17 04:48:01 +0900

    Client 추가, 수정 추가 - 서버 연결 상태를 내보내는 스트림 수정 1. 메인 클래스에서 모두 실행 1.1.생성자 삭제 1.2.불필요한run()메소드 삭제 2. Thread 분리 3. writeObject를 두 번 하던 것 삭제(생성자삭제와 함께)

--- dragonball/src/com/dae/dragonball/client/Client.java
+++ dragonball/src/com/dae/dragonball/client/Client.java
@@ -1,12 +1,9 @@
 package com.dae.dragonball.client;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.DataInputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-import com.dae.dragonball.Play;
 import com.dae.dragonball.PlayerVO;
 
 public class Client {
@@ -17,101 +14,38 @@ public class Client {
         String serverIp = "127.0.0.1";
         int port = 7777;
         Socket socket;
-        Play play = new Play();
         PlayerVO player;
         Boolean isBot;
         String playerName;
         int hp;
+
         isBot = false;
         System.out.println("이름 입력");
         playerName = scanner.next();
         hp = 3;
         player = new PlayerVO(isBot, playerName, hp);
-        System.out.println("1client");
 
         try {
-
-            System.out.println("2client");
-
             socket = new Socket(serverIp, port);
-            System.out.println("서버와 연결되었습니다");
-            Thread sender = new Thread(new ClientSender(socket, player));
-            Thread receiver = new Thread(new ClientReceiver(socket));
-            sender.start();
-            receiver.start();
+
+            /*
+             * TODO: 서버에서 error 메세지 띄우면 종료하도록 읽기
+             */
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            System.out.println(dis.readUTF());
+
+            ClientThread clientThread = new ClientThread(socket, player);
+            Thread thread = new Thread(clientThread);
+            while (true) {
+                thread.start();
+            }
 
         } catch (Exception e) {
-            // TODO: handle exception
         } finally {
 
-        }
-    }
+            System.out.println("경기를 종료합니다.");
+            System.exit(0);
 
-    static class ClientSender implements Runnable {
-        Socket socket;
-        ObjectOutputStream oos;
-
-        PlayerVO player;
-        Boolean isBot;
-        String playerName;
-        int hp;
-
-        public ClientSender(Socket socket, PlayerVO player) {
-            this.socket = socket;
-            this.player = player;
-
-            try {
-                oos = new ObjectOutputStream(socket.getOutputStream());
-                oos.writeObject(player);
-                oos.flush();
-            } catch (Exception e) {
-            }
-        }
-
-        @Override
-        public void run() {
-
-            System.out.println("csrun");
-            Play.clientSender(player);
-            try {
-                oos.writeObject(player);
-                oos.flush();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-
-    }
-
-    static class ClientReceiver extends Thread {
-        Socket socket;
-        ObjectInputStream ois;
-        PlayerVO player;
-
-        public ClientReceiver(Socket socket) {
-            this.socket = socket;
-            try {
-                ois = new ObjectInputStream(socket.getInputStream());
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
-        }
-
-        public synchronized void run() {
-            try {
-
-                player = (PlayerVO) ois.readObject();
-                System.out.println("crrun");
-                Play.printMessage(player);
-            } catch (ClassNotFoundException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } finally {
-            }
         }
     }
 
