814c4921451ece8f730ef3b05366bceafb6f3d89
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2019-04-17 04:54:17 +0900

    ServerMulti 생성 기존 서버의 문제점 해결 1. 기존 서버의 멀티 스레드로 인한 데이터 분리 ->sender와 receiver 병합 2. 싱글 스레드 서버에서 소켓 초기화 문제 ->스레드별로 클라이언트를 하나씩 할당

---
+++ dragonball/src/com/dae/dragonball/server/ServerMulti.java
@@ -0,0 +1,74 @@
+package com.dae.dragonball.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+
+import com.dae.dragonball.PlayerVO;
+
+public class ServerMulti {
+    private HashMap<String, ObjectOutputStream> playerMap;
+    private ArrayList<PlayerVO> players;
+    ServerSocket serverSocket;
+    Socket socket;
+    ObjectInputStream ois;
+    ObjectOutputStream oos;
+    Set<String> keySet;
+
+    public ServerMulti() {
+        playerMap = new HashMap<String, ObjectOutputStream>();
+        players = new ArrayList<PlayerVO>();
+
+        Collections.synchronizedMap(playerMap);
+        Collections.synchronizedList(players);
+
+    }
+
+    public static void main(String[] args) {
+        new ServerMulti().start();
+    }
+
+    public void start() {
+
+        try {
+            serverSocket = new ServerSocket(7777);
+
+            System.out.println("서버 초기화");
+            int cnt = 0;
+            while (true) {
+                System.out.println("접속자 수 : " + cnt);
+
+                socket = serverSocket.accept();
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                if (cnt < 2) {
+                    dos.writeUTF("[Notice]서버와 연결되었습니다");
+                } else {
+                    dos.writeUTF("[Error]서버가 가득 찼습니다.");
+                    return;
+                }
+                cnt++;
+                ServerThread serverReceiver = new ServerThread(socket, playerMap, players);
+                Thread thread = new Thread(serverReceiver);
+                thread.start();
+                if (dis.readUTF() != null) {
+                    /*
+                     * 클라이언트가 나가도 서버가 종료되지 않도록 하려면? 재귀호출로 다시 시작하도록?
+                     */
+                    cnt--;
+                }
+
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+}
